b60dcf67f3e27870f68cb70384e877bc0698ba2c
HristoDikov? <dikoww7@gmail.com>
2020-03-29 22:53:20 +0300

    Create database models

--- RentItEasy.Data/ApplicationDbContext.cs
+++ RentItEasy.Data/ApplicationDbContext.cs
@@ -1,6 +1,7 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using AspNetCoreTemplate.Data.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.VisualStudio.Services.Account;
+using RentItEasy.Models;
 
 namespace RentItEasy.Data
 {
@@ -11,13 +12,31 @@ namespace RentItEasy.Data
         {
         }
 
-        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        public DbSet<Account> Accounts { get; set; }
+
+        public DbSet<Ad> Ads { get; set; }
+
+        public DbSet<AgencyProfile> AgenciesProfiles { get; set; }
+
+        public DbSet<Property> Properties { get; set; }
+
+        public DbSet<Rating> Ratings { get; set; }
+
+        public DbSet<UserProfile> UsersProfiles { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
         {
-            if (!optionsBuilder.IsConfigured)
-            {
-                optionsBuilder
-                    .UseSqlServer(Configuration.ConnectionString);
-            }
+            base.OnModelCreating(builder);
+
+            builder.Entity<Account>()
+                   .HasOne(a => a.AgencyProfile)
+                   .WithOne(c => c.Account)
+                   .HasForeignKey<AgencyProfile>(c => c.AccountId);
+
+            builder.Entity<Account>()
+                   .HasOne(a => a.UserProfile)
+                   .WithOne(u => u.Account)
+                   .HasForeignKey<UserProfile>(u => u.AccountId);
         }
     }
 }
--- RentItEasy.Web/Startup.cs
+++ RentItEasy.Web/Startup.cs
@@ -30,11 +30,15 @@ namespace RentItEasy
             services.AddDbContext<ApplicationDbContext>(options =>
                 options.UseSqlServer(
                     Configuration.GetConnectionString("DefaultConnection")));
+
             services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
                 .AddEntityFrameworkStores<ApplicationDbContext>();
+
             services.AddControllersWithViews();
+
             services.AddRazorPages();
         }
+       
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
